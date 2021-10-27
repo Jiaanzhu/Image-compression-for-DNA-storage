947799fb8813e512a932cba11e24379b63e5a97d
Jiaanzhu <58867544+Jiaanzhu@users.noreply.github.com>
2021-10-27 13:17:49 +0200

    Add files via upload

--- jpegdna_eval_Jiaan.py
+++ jpegdna_eval_Jiaan.py
@@ -10,18 +10,19 @@ from pandas import ExcelWriter
 import jpegdna
 from jpegdna.codecs import JPEGDNA
 from IQA_pytorch import SSIM, MS_SSIM, VIF
-import numpy as np
 import torch
+import numpy as np
 
 def stats(func):
     """Stats printing and exception handling decorator"""
 
     def inner(*args):
         try:
-            code, decoded = func(*args)
+            code, decoded_pad = func(*args)
         except ValueError as err:
             print(err)
         else:
+            decoded = decoded_pad[:img.shape[0], :img.shape[1]] 
             compression_rate = 8 * img.shape[0] * img.shape[1] / len(code)            
             
             #Calculate MSE and PSNR
@@ -34,28 +35,29 @@ def stats(func):
             #D_3 = VIF()
             
             #To get 4-dimension torch tensors
-            torch_decoded = torch.FloatTensor(decoded).unsqueeze(0).unsqueeze(0)
-            torch_img = torch.FloatTensor(img).unsqueeze(0).unsqueeze(0)
+            torch_decoded = torch.FloatTensor(decoded.astype(int)).unsqueeze(0).unsqueeze(0)
+            torch_img = torch.FloatTensor(img.astype(int)).unsqueeze(0).unsqueeze(0)
             
             #Calculate SSIM, MS-SSIM, VIF
-            SSIM_r = D_1(torch_decoded , torch_img, as_loss=False)
-            MS_SSIM_r = D_2(torch_decoded, torch_img)
+            SSIM_value = D_1(torch_decoded , torch_img, as_loss=False)
+            MS_SSIM_value = D_2(torch_decoded, torch_img, as_loss=False)
             #VIF_r = D_3(torch_decoded, torch_img)
             
             #Print out the results
             print(f"Mean squared error: {MSE}")
             print(f"PSNR: {PSNR}")
-            print(f"SSIM: {SSIM_r}")
-            print(f"MS_SSIM: {MS_SSIM_r}")
+            print(f"SSIM: {SSIM_value}")
+            print(f"MS_SSIM: {MS_SSIM_value}")
             #print(f"VIF: {VIF_r}")
             print(f"Compression rate: {compression_rate} bits/nt")
             # io.imsave(str(compression_rate) + ".png", decoded)
-            return compression_rate, PSNR, SSIM_r, MS_SSIM_r
+            return compression_rate, PSNR, SSIM_value, MS_SSIM_value
     return inner
 
 def encode_decode(img, alpha):
     """Function for encoding and decoding"""
-    choice = "from_img"
+    choice = "default"
+    # Coding
     codec = JPEGDNA(alpha, verbose=False, verbosity=3)
     if choice == "from_img":
         (code, res) = codec.full_encode(img, "from_img")
@@ -63,8 +65,18 @@ def encode_decode(img, alpha):
         with open(Path(jpegdna.__path__[0] + "/data/freqs.pkl"), "rb") as file:
             freqs = pickle.load(file)
         (code, res) = codec.full_encode(img, "from_file", freqs['freq_dc'], freqs['freq_ac'])
+    elif choice == "default":
+        (code, res) = codec.full_encode(img, "default")
+    # Decoding
     codec2 = JPEGDNA(alpha, verbose=False, verbosity=3)
-    decoded = codec2.full_decode(code, res[1], res[2], res[3], res[4])
+    if choice == "from_img":
+        decoded = codec2.full_decode(code, "from_img", res[1], res[2], res[3], res[4])
+    elif choice == "from_file":
+        with open(Path(jpegdna.__path__[0] + "/data/freqs.pkl"), "rb") as file:
+            freqs = pickle.load(file)
+        decoded = codec2.full_decode(code, "from_file", res[1], res[2], freqs['freq_dc'], freqs['freq_ac'])
+    elif choice == "default":
+        decoded = codec2.full_decode(code, "default", res[1], res[2])
     return code, decoded
 
 @stats
@@ -74,26 +86,29 @@ def experiment(img, alpha):
 
 if __name__ == '__main__':
     value = make_dataclass("value", [("Compressionrate", float), ("PSNR", float)
-                                     ,("SSIM_r", float), ("MS_SSIM_r", float)])
+                                     ,("SSIM_value", float), ("MS_SSIM_value", float)])
     general_results = []
-    img_names = ["kodim_gray_1.png"]
-    #img_names = ["1_gray.jpg", "2_gray.jpg", "3_gray.jpg", "4_gray.jpg", "5_gray.jpg", "6_gray.jpg", "7_gray.jpg", "8_gray.jpg"]
+    img_names = ["test.jpeg"]
+    #img_names = ["kodim_gray_1.png", "kodim_gray_2.png", "kodim_gray_3.png", "kodim_gray_4.png", "kodim_gray_5.png"]
     for i in range(len(img_names)):
         IMG_NAME = img_names[i]
-        img = io.imread("img/" + IMG_NAME)
-        img = img[:8*(img.shape[0]//8), :8*(img.shape[1]//8)]
+        img = io.imread(Path(jpegdna.__path__[0] + "/../img/" + IMG_NAME))
+        #img = img[:160, :160, 0]
+        img_pad = np.zeros((int(8*(np.ceil(img.shape[0]/8))), int(8*(np.ceil(img.shape[1]/8)))))
+        img_pad[:img.shape[0], :img.shape[1]] = img
+        #img = img[:8*(img.shape[0]//8), :8*(img.shape[1]//8)]
         # import matplotlib.pyplot as plt
         values = []
-        for alpha in [1e-5, 0.145, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-        #for alpha in [0.5]:
+        for alpha in [0.2, 0.4, 0.6, 0.8, 1]:
+        #for alpha in [1e-5, 0.145, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
             print("==================================")
             print(f"Alpha: {alpha}")
-            res = experiment(img, alpha)
+            res = experiment(img_pad, alpha)
             if res is not None:
-                compression_rate, PSNR, SSIM_r, MS_SSIM_r = res
-                values.append(value(compression_rate, PSNR, SSIM_r, MS_SSIM_r))
+                compression_rate, PSNR, SSIM_value, MS_SSIM_value = res
+                values.append(value(compression_rate, PSNR, SSIM_value, MS_SSIM_value))
         general_results.append(values)
-   #with ExcelWriter("res/results.xlsx") as writer: # pylint: disable=abstract-class-instantiated
-    #    for i in range(len(general_results)):
-     #       dtf = pd.DataFrame(general_results[i])
-      #      dtf.to_excel(writer, sheet_name=img_names[i], index=None, header=True) 
+    with ExcelWriter("results/results.xlsx") as writer: # pylint: disable=abstract-class-instantiated
+        for i in range(len(general_results)):
+            dtf = pd.DataFrame(general_results[i])
+            dtf.to_excel(writer, sheet_name=img_names[i], index=None, header=True)
